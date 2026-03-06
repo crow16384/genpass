687d537a0385bb99cd6b0b2881b10a9ab6a9393a
Vladimir Larchenko <vladimir.larchenko@keystatsolutions.com>
2026-03-06 08:37:30 +0300

    Add SPDX GPL-3.0 headers to source files

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
 #include <boost/program_options.hpp>
 #include <cstdlib>
 #include <fstream>
--- src/genlib/randomer.cpp
+++ src/genlib/randomer.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
 #include "randomer.hpp"
 
 Rand::Rand(std::size_t max)
